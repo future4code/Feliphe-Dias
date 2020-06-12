6e3b85f2d413c7e64641fd7f2b188294953c0f9c
feliperdi <dias.rodrigues.feliphe@gmail.com>
2020-06-12 13:02:48 -0300

    exercicio 1

---
+++ semana-4/lista-de-exercicios/index.js
@@ -0,0 +1,13 @@
+/*
+    INTERPRETAÇÃO DE CODIGO
+    
+    1:    
+    a função recebe como parametro valorEmDolar.
+    ela captura um prompt e converte o mesmo para Number e o armazena na variavel cotacao
+    e retorna um string contendo "R$" concatenado com o produto de valorEmDOlar*Cotacao
+
+    resultado no console/browser:
+        prompt---->cotação informada 5,02 reais
+        console -- > R$ 502,00
+
+*/
