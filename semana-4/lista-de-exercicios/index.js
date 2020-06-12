5f1708ad662c0ec86c933c3e7c0e8d3a0a50fe36
feliperdi <dias.rodrigues.feliphe@gmail.com>
2020-06-12 17:24:53 -0300

    exercicio FUNCOES DE ARRAYS  5

--- semana-4/lista-de-exercicios/index.js
+++ semana-4/lista-de-exercicios/index.js
@@ -312,3 +312,23 @@ const array = [1, 2, 3, 4, 5, 6];
 //      });  
 //      return email;
 // }
+
+// // 5
+// const contas = [
+// 	{ cliente: "João", saldoTotal: 1000, compras: [100, 200, 300] },
+// 	{ cliente: "Paula", saldoTotal: 7500, compras: [200, 1040] },
+// 	{ cliente: "Pedro", saldoTotal: 10000, compras: [5140, 6100, 100, 2000] },
+// 	{ cliente: "Luciano", saldoTotal: 100, compras: [100, 200, 1700] },
+// 	{ cliente: "Artur", saldoTotal: 1800, compras: [200, 300] },
+// 	{ cliente: "Soter", saldoTotal: 1200, compras: [] }
+// ]
+
+// function atualizaSaldo(o){
+//    o.forEach((c, i, a)=>{
+//          return c.compras.length > 0 ? c.saldoTotal -= c.compras.reduce((total,num)=>{ return total+num}) : false;
+//     })
+//     return o;
+// }
+
+
+// console.log(atualizaSaldo(contas));
