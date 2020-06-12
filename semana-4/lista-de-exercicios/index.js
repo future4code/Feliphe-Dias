1757a120bbbcb80446526914c5515a360f122fab
feliperdi <dias.rodrigues.feliphe@gmail.com>
2020-06-12 15:41:53 -0300

    exercicio FUNCOES DE ARRAYS  2

--- semana-4/lista-de-exercicios/index.js
+++ semana-4/lista-de-exercicios/index.js
@@ -232,13 +232,12 @@
 // }
 
 //2:
-//a
-const array = [1, 2, 3, 4, 5, 6];
-function triplo(a){
-    a = a.map((n,i,a)=>{
-        return n*3;
-    })
-    return a;
-}
+// //a
+// const array = [1, 2, 3, 4, 5, 6];
+// function triplo(a){
+//     a = a.map((n,i,a)=>{
+//         return n*3;
+//     })
+//     return a;
+// }
 
-console.log(triplo(array));
