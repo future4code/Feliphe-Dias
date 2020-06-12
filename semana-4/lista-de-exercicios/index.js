4821830964539baf6ee6d2816fd06fa4699de3b0
feliperdi <dias.rodrigues.feliphe@gmail.com>
2020-06-12 13:03:24 -0300

    exercicio 2

--- semana-4/lista-de-exercicios/index.js
+++ semana-4/lista-de-exercicios/index.js
@@ -10,4 +10,19 @@
         prompt---->cotação informada 5,02 reais
         console -- > R$ 502,00
 
+       2:
+    a função recebe como parametro um string e um number
+    o switch compara o parametro string
+    para cada case do switch multiplica o parametro number por um valor diferente
+    e o adiciona em uma variavel chama valorAposInvestimento
+    caso  o parametro string não corresponda a nenhum case executa o default
+    gerando um alert no navegor informando o usuario que o tipo de investimento está incorreto
+
+    e por fim retorna a variavel valorAposInvestimento
+
+    resultado no console/browser:
+        console ----- > 165
+        alert ----> TIPO DE INVESTIMENTO INFORMADO INCORRETO
+
+
 */
